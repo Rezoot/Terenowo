0746a7b1b4d494c57f6d851fe9b8749a852e532a
Rezoot <rezacz2002@gmail.com>
2023-12-24 21:03:14 +0100

    zamykanie apki, nowe menu, nowy czas

--- src/Menu.java
+++ src/Menu.java
@@ -125,7 +125,7 @@ public class Menu extends Okno{
         center.add(Box.createVerticalStrut(30));
         center.add(wyb);
 
-        graj.addActionListener(e -> {Gra glowna = new Gra(time);});
+        graj.addActionListener(e -> {new Gra(time);pulpit.dispose();});
 
     }
     JPanel jradiobutony(){
