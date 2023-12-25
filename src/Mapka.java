56a47abf904cdb7ed860767e74d3eafdf63983e9
Rezoot <rezacz2002@gmail.com>
2023-12-25 21:21:07 +0100

    mapka???

--- src/Mapka.java
+++ src/Mapka.java
@@ -1,18 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Mapka extends Okno{
 
-
     Mapka(){
 
         pulpit.setSize(100,100);
-
         pulpit.setBounds(szerokoscokna-400,wysokoscokna-460,400,400);
         pulpit.setAlwaysOnTop(true);
-        
-        JButton zatwierdz = new JButton("zatwierdz");
+        pulpit.setLayout(new BorderLayout());
+        //pulpit.setBackground(new Color(0,0,0,0));
+
         JPanel dol = new JPanel();
+        dol.setLayout(new BorderLayout());
+        dol.setOpaque(false);
+
+        JButton zatwierdz = new JButton("zatwierdz");
+
+        pulpit.add(dol,BorderLayout.SOUTH);
+        dol.add(zatwierdz,BorderLayout.EAST);
 
 
         pulpit.setVisible(false);
@@ -20,7 +27,6 @@ public class Mapka extends Okno{
 
     void chowanie(Boolean bool){
         pulpit.setVisible(bool);
-
 
     }
 
