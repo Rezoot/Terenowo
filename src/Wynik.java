c6977890599c60ad6f4954c1ec35a031534f568b
Rezoot <rezacz2002@gmail.com>
2024-01-03 13:04:14 +0100

    powoli do wyniku

--- src/Wynik.java
+++ src/Wynik.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.util.concurrent.ForkJoinPool;
 
 public class Wynik extends Okno{
     String gra;
@@ -12,6 +11,7 @@ public class Wynik extends Okno{
     String czas;
     double x1,x2,y1,y2;
     int tryb;
+    int[] tablica = new int[3];
     Wynik(int Tryb, String Czas,String Gra,double X1,double Y1,double X2,double Y2,int Punkty) {
         tryb=Tryb;
         punkty=Punkty;
@@ -28,6 +28,13 @@ public class Wynik extends Okno{
         mappa();
 
         dol();
+
+        dalej();
+
+        JPanel gora = new JPanel();
+        pulpit.add(gora);
+        gora.setOpaque(false);
+        gora.setPreferredSize(new Dimension(szerokoscokna,wysokoscokna-mapa-5*tekst));
 
 
         pulpit.setVisible(true);
@@ -63,7 +70,7 @@ public class Wynik extends Okno{
         gora.add(plansza);
         plansza.add(zamknij,BorderLayout.EAST);
 
-        zamknij.addActionListener(e -> {pulpit.dispose();});
+        zamknij.addActionListener(e -> pulpit.dispose());
 
 
 
@@ -81,7 +88,9 @@ public class Wynik extends Okno{
         wynik.setOpaque(false);
         gora.add(wynik);
 
-        plus=punkty();
+
+        tablica=punkty();
+        plus=tablica[2];
         punkty+=plus;
 
         JLabel odliczanie = new JLabel("wynik: "+punkty);
@@ -161,7 +170,7 @@ public class Wynik extends Okno{
         pulpit.add(dol2);
         dol2.setOpaque(false);
         dol2.setBackground(Color.blue);
-        dol2.setPreferredSize(new Dimension(szerokoscokna,wysokoscokna-mapa-3*tekst));
+        dol2.setPreferredSize(new Dimension(szerokoscokna,tekst));
 
         JPanel odl = new JPanel();
         wynik.setOpaque(false);
@@ -175,7 +184,7 @@ public class Wynik extends Okno{
         }
         else
         {
-             napis = "odleglosc: " + (int)mapka.odleglosc + "m";;
+             napis = "odleglosc: " + (int)mapka.odleglosc + "m";
         }
 
         JLabel km = new JLabel(napis);
@@ -187,13 +196,90 @@ public class Wynik extends Okno{
         odl.add(km,BorderLayout.CENTER);
     }
 
-    int punkty(){
-        int punkty;
-        punkty = 5000 - (int)Math.round(mapka.odleglosc)+1000/tryb*30;
+    void dalej(){
+        JPanel gora = new JPanel();
+        pulpit.add(gora);
+        gora.setLayout(new OverlayLayout(gora));
+        gora.setOpaque(false);
+        gora.setPreferredSize(new Dimension(szerokoscokna,2*tekst));
 
-        if(punkty<0){punkty=0;}
-        System.out.println(tryb);
-        return punkty;
+
+        JPanel wynik = new JPanel();
+        wynik.setOpaque(false);
+        gora.add(wynik);
+
+        if(plus!=0){
+            JPanel dodatkowy =new JPanel();
+            dodatkowy.setLayout(new BoxLayout(dodatkowy,BoxLayout.Y_AXIS));
+            dodatkowy.setOpaque(false);
+            wynik.add(dodatkowy);
+
+            String tek = "+" + tablica[0];
+            JLabel pun = new JLabel(tek);
+            pun.setFont(new Font("Arial", Font.PLAIN, 30));
+            dodatkowy.add(pun);
+
+            tek = "+" + tablica[1];
+            JLabel pun2 = new JLabel(tek);
+            pun2.setFont(new Font("Arial", Font.PLAIN, 30));
+            dodatkowy.add(pun2);
+
+           new Thread(()->{
+
+               try {
+                   Thread.sleep(1000);
+                   for(int i=255;i>=0;i--){
+                      pun.setForeground(new Color(0,0,0,i));
+                      pun2.setForeground(new Color(0,0,0,i));
+                      Thread.sleep(10);
+                   }
+
+               } catch (InterruptedException e) {
+                   System.out.println("Ddddd");
+                   throw new RuntimeException(e);
+
+               }
+
+
+           }).start();
+
+
+        }
+
+
+
+
+        JPanel plansza = new JPanel();
+        plansza.setOpaque(false);
+        plansza.setLayout(new BorderLayout());
+
+
+
+        gora.add(plansza);
+
+        JButton dalej =  new JButton();
+        dalej.setPreferredSize(new Dimension(70,50));
+
+        plansza.add(dalej,BorderLayout.EAST);
+
+        //zamknij.addActionListener(e -> {pulpit.dispose();});
+
+
+
+
+    }
+
+
+    int[] punkty(){
+        int punkty;
+        int[] c = new int[3];
+        int a = (int)Math.round(mapka.odleglosc);
+        punkty = 5000 - a;
+        c[0]=punkty;
+
+        if(punkty<0){punkty=0;c[0]=0;} else if (punkty<2000){punkty+=500/tryb*30;c[1]=500/tryb*30;}else{punkty+=1000/tryb*30;c[1]=1000/tryb*30;}
+        c[2]=punkty;
+        return c;
     }
 
 }
